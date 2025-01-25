47722a8e76af2e5506b5b3a8858d163d16dbf7f4
Jagjeet Dangar <jagjeet.dangar.cg@gmail.com>
2025-01-25 12:14:28 +0530

    Made 1 post and 1 get request and removed node_modules from github.

--- Flex_mate backend/index.js
+++ Flex_mate backend/index.js
@@ -24,6 +24,18 @@ app.use(express.json());
       }
     });
     
+    app.get("/likes", async (req, res) => {
+      try {
+        const userId = req.query.userId; 
+        const filter = userId ? { userId: userId } : {}; 
+        const users = await db.collection("likes").find(filter).toArray(); 
+        res.json(users); 
+      } catch (error) {
+        console.error(error); 
+        res.status(500).json({ error: "An error occurred while fetching likes." });
+      }
+    });
+    
 
     app.listen(port, () => console.log(`Server running on http://localhost:${port}`));
   } catch (err) {
