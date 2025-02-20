fce8e8754f1edd98e277de1f73ed881858db29e5
Jagjeet Dangar <jagjeet.dangar.cg@gmail.com>
2025-02-20 10:22:02 +0530

    Did the front-end work in this website.

--- Flex_mate frontend/flex_mate_frontend/src/Portfolio.jsx
+++ Flex_mate frontend/flex_mate_frontend/src/Portfolio.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import './portfolio.css';
 import customers from './assets/customers.png';
+import skillsphoto from './assets/groupphoto2.png';
 
 export default function Portfolio() {
   const [data, setData] = useState([]);
@@ -30,36 +31,40 @@ export default function Portfolio() {
           <h3 className="count-2">Happy Customers</h3>
         </div>
       ))}
+      <div className="portfolio-container-2">
+        <img src={skillsphoto} alt="" className="neel-sir-and-me" />
+        <div className="skillstext">INCREASE YOUR SKILLS & IMPACT</div>
+      </div>
     </div>
   );
 }
 
 
 
-    // <div className="portfolio-container">
-    //   <header className="hero-section">
-    //     <h1>{data.Header}</h1>
-    //     <p>{data.WorkDescription}</p>
-    //     <img src={data.YourimageLink} alt="Profile" className="profile-image" />
-    //   </header>
+// <div className="portfolio-container">
+//   <header className="hero-section">
+//     <h1>{data.Header}</h1>
+//     <p>{data.WorkDescription}</p>
+//     <img src={data.YourimageLink} alt="Profile" className="profile-image" />
+//   </header>
 
-    //   <section className="skills-section">
-    //     <h2>{data.SkillsHeader}</h2>
-    //     <p>{data.SkillsHeaderDescription}</p>
-    //     <div className="skills">
-    //       {[data["1stskill"], data["2ndskill"], data["3rdskill"]].map((skill, index) => (
-    //         <div key={index} className="skill-card">
-    //           <h3>{skill[0]}</h3>
-    //           <p>{skill[1]}</p>
-    //         </div>
-    //       ))}
-    //     </div>
-    //   </section>
+//   <section className="skills-section">
+//     <h2>{data.SkillsHeader}</h2>
+//     <p>{data.SkillsHeaderDescription}</p>
+//     <div className="skills">
+//       {[data["1stskill"], data["2ndskill"], data["3rdskill"]].map((skill, index) => (
+//         <div key={index} className="skill-card">
+//           <h3>{skill[0]}</h3>
+//           <p>{skill[1]}</p>
+//         </div>
+//       ))}
+//     </div>
+//   </section>
 
-    //   <section className="project-section">
-    //     <h2>See My Latest Project</h2>
-    //     <a href={data.YourLatestProjectLink} target="_blank" rel="noopener noreferrer">
-    //       <p>{data.YourLatestProjectLinkDescription}</p>
-    //     </a>
-    //   </section>
-    // </div>
+//   <section className="project-section">
+//     <h2>See My Latest Project</h2>
+//     <a href={data.YourLatestProjectLink} target="_blank" rel="noopener noreferrer">
+//       <p>{data.YourLatestProjectLinkDescription}</p>
+//     </a>
+//   </section>
+// </div>
